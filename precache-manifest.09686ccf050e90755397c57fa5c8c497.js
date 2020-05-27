b3029b4a4e01aff8b54190c838890b3843d5b5c0
Allen Lee <allenlee@mit.edu>
2020-05-27 19:48:32 -0400

    Updates

---
+++ precache-manifest.09686ccf050e90755397c57fa5c8c497.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb271e143ef3454bfb0d789e8ff6048",
+    "url": "/memorability_thumbnailing_experiment/index.html"
+  },
+  {
+    "revision": "c97e73ac21d23dc1b352",
+    "url": "/memorability_thumbnailing_experiment/static/css/2.55146207.chunk.css"
+  },
+  {
+    "revision": "629979d63f1ce40a99ad",
+    "url": "/memorability_thumbnailing_experiment/static/css/main.0c50e872.chunk.css"
+  },
+  {
+    "revision": "c97e73ac21d23dc1b352",
+    "url": "/memorability_thumbnailing_experiment/static/js/2.a452f0fe.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "/memorability_thumbnailing_experiment/static/js/2.a452f0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629979d63f1ce40a99ad",
+    "url": "/memorability_thumbnailing_experiment/static/js/main.65e0d6b4.chunk.js"
+  },
+  {
+    "revision": "001ad928043e9f1bd9ba",
+    "url": "/memorability_thumbnailing_experiment/static/js/runtime-main.86fb9ae7.js"
+  }
+]);
